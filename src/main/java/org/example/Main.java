4259a48b1866e820eca5c44e67b059c50885fa2e
George60229 <pashnev.mega@gmail.com>
2022-12-22 20:24:30 +0200

    add jar from gradle creating

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         StringUtils utils=new StringUtils();
 
-        System.out.println(utils.isPositiveNumber("Hello"));
+        System.out.println(utils.isPositiveNumber("-6"));
     }
 }
--- src/test/java/TestStringUtils.java
+++ src/test/java/TestStringUtils.java
@@ -1,18 +1,25 @@
 import org.example.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestStringUtils {
-    @Test
-    public void stringUtils() {
-        StringUtils utils=new StringUtils();
-        assertFalse(utils.isPositiveNumber("-4"));
-        assertFalse(utils.isPositiveNumber("-6"));
-        assertTrue(utils.isPositiveNumber("7"));
-        assertTrue(utils.isPositiveNumber("100"));
-        assertFalse(utils.isPositiveNumber("Hello"));
+    StringUtils utils = new StringUtils();
+
+    @ParameterizedTest(name = "expected false")
+    @ValueSource(strings = {"-3", "pop", "-5", "-7", "-128"})
+    public void isPositiveFalse(String number) {
+        assertFalse(utils.isPositiveNumber(number));
+
+    }
+
+    @ParameterizedTest(name = "expected true")
+    @ValueSource(strings = {"3", "100", "5", "7", "128"})
+    public void isPositiveTrue(String number) {
+        assertTrue(utils.isPositiveNumber(number));
 
     }
 }
